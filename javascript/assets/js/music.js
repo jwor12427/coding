7c6d67eb973a63981e7652400c3614ec5d2afeec
jwor12427 <109262155+jwor12427@users.noreply.github.com>
2022-10-13 11:20:21 +0900

    2022.10.13

--- javascript/assets/js/music.js
+++ javascript/assets/js/music.js
@@ -61,7 +61,7 @@ const allMusic = [
     },
 ]
 
-//서택자
+//선택자
 const musicWrap = document.querySelector(".music__wrap");
 const musicView = musicWrap.querySelector(".music__view .img img");
 const musicName = musicWrap.querySelector(".music__view .title h3");
@@ -79,14 +79,15 @@ const musicListBtn = musicWrap.querySelector("#control-list");
 const musicList = musicWrap.querySelector(".music__list");
 const musicListUl = musicWrap.querySelector(".music__list ul");
 
-let musicIndex = 1;
+let musicIndex = 1; //처음 음악
 
 //음악 재생
 function loadMusic(num){
-    musicName.innerText = allMusic[num-1].name;
-    musicArtist.innerText = allMusic[num-1].artist;
-    musicView.src = `../assets/img/`+allMusic[num-1].img+`.png`;
-    musicAudio.src = `../assets/audio/`+allMusic[num-1].audio+`.mp3`;
+    musicName.innerText = allMusic[num-1].name;         //음악 이름 로드
+    musicArtist.innerText = allMusic[num-1].artist;     //음악 아티스트 로드
+    musicView.src = `../assets/img/`+allMusic[num-1].img+`.png`;    //음악 이미지 로드  
+    musicView.alt = allMusic[num-1].name;                               //음악 alt태그
+    musicAudio.src = `../assets/audio/`+allMusic[num-1].audio+`.mp3`;   //음악 
 }
 
 //재생 버튼
@@ -110,6 +111,7 @@ function prevMusic(){
     musicIndex == 1 ? musicIndex = allMusic.length : musicIndex--;
     loadMusic(musicIndex);
     playMusic();
+    playListMusic();  
 }
 
 //다음곡 듣기 버튼
@@ -117,6 +119,7 @@ function nextMusic(){
     musicIndex == allMusic.length ? musicIndex = 1 : musicIndex++;
     loadMusic(musicIndex);
     playMusic();
+    playListMusic();
 }
 
 //뮤직 진행바
@@ -173,7 +176,7 @@ musicRepeat.addEventListener("click", () => {
             musicRepeat.setAttribute("title", "전체 재생");
         break;
     }
-})
+});
 
 //오디오가 끝나면
 musicAudio.addEventListener("ended", () => {
@@ -197,25 +200,8 @@ musicAudio.addEventListener("ended", () => {
             playMusic();                        //로드한 음악을 재생
         break;
     };
+    playListMusic();    //재생목록 업데이트
 });
-
-//뮤직 리스트 버튼 클릭
-musicListBtn.addEventListener("click", () => {
-    musicList.classList.add("show");
-});
-
-//뮤직 리스트 구현하기
-for(let i=0; i<allMusic.length; i++){
-    let li = `
-    <li>
-        <strong>${allMusic[i].name}</strong>
-        <em>${allMusic[i].artist}</em>
-        <span></span>
-    </li>`;
-
-    musicListUl.innerHTML += li;
-}
-
 
 //플레이 버튼
 musicPlay.addEventListener("click", () => {
@@ -231,9 +217,69 @@ musicNextBtn.addEventListener("click", () => {
     nextMusic();
 });
 
+//뮤직 리스트 버튼 클릭
+musicListBtn.addEventListener("click", () => {
+    musicList.classList.add("show");
+});
+
+//뮤직 리스트 구현하기
+for(let i=0; i<allMusic.length; i++){
+    let li = `
+    <li data-index="${i+1}">
+        <strong>${allMusic[i].name}</strong>
+        <em>${allMusic[i].artist}</em>
+        <audio class="${allMusic[i].audio}" src="../assets/audio/${allMusic[i].audio}.mp3"></audio>
+        <span class="audio-duration" id="${allMusic[i].audio}">재생시간</span>
+    </li>`;
+
+    // musicListUl.innerHTML += li;        //추가 시켜주는 것 - 한번에 로딩이 되기 때문에 인식x
+    musicListUl.insertAdjacentHTML("beforeend", li);    //차곡차곡 데이터가 쌓여감 - 로딩 방식이 다름, 데이터가 쌓이고 로딩이 되기때문에 데이터 인식
+
+    //리스트에 음악 시간 불러오기
+    let liAudioDuration = musicListUl.querySelector(`#${allMusic[i].audio}`);   //리스트에서 시간을 표시할 선택자를 가져옴
+    let liAudio = musicListUl.querySelector(`.${allMusic[i].audio}`);           //리스트에서 오디오를 가져옴
+    liAudio.addEventListener("loadeddata", () => {
+        let audioDuration = liAudio.duration;        //오디오 전체 길이
+        let totalMin = Math.floor(audioDuration / 60);  //전체 길이를 분 단위로 쪼갬
+        let totalSec = Math.floor(audioDuration % 60);  //초 계산
+        if(totalSec < 10) totalSec = `0${totalSec}`;    //앞자리에 0 추가
+        liAudioDuration.innerText = `${totalMin}:${totalSec}`;   //문자열 출력
+        liAudioDuration.setAttribute("data-duration", `${totalMin}:${totalSec}`);   //속성에 오디오 길이 기록
+    });
+}
+
+//뮤직 리스트를 클릭하면 재생
+function playListMusic(){
+    const musicListAll = musicListUl.querySelectorAll("li");    //뮤직 리스트 목록
+    for(let i=0; i<musicListAll.length; i++){
+        let audioTag = musicListAll[i].querySelector(".audio-duration");
+
+        if(musicListAll[i].classList.contains("playing")){
+            musicListAll[i].classList.remove("playing");        //클래스가 존재하면 삭제
+            let adDuration = audioTag.getAttribute("data-duration");    //오디오 길이 값 가져오기
+            audioTag.innerText = adDuration;                //오디오 길이 값 출력
+        }
+        if(musicListAll[i].getAttribute("data-index") == musicIndex){  //련재 뮤직 인덱스랑 리스트 인덱스 값이 같으면 재생
+            musicListAll[i].classList.add("playing");                  //클래스 playing 추가
+            audioTag.innerText = "재생중";                              //재생중일 경우 재생중 멘트 추가
+        }
+
+        musicListAll[i].setAttribute("onclick", "clicked(this)");
+    }
+};
+
+//뮤직 리스트를 클릭하면 정보가 넘어감 - 재생
+function clicked(el){
+    let getLiIndex = el.getAttribute("data-index");     //클릭한 리스트의 인덱스값을 저장
+    musicIndex = getLiIndex;                            //클릭한 인덱스 값을 뮤직 인덱스 저장
+    loadMusic(musicIndex);                              //해당 인덱스 뮤직 로드
+    playMusic();                                        //음악 재생
+    playListMusic();                                    //음악 리스트 업데이트
+};
 
 
 //음악 재생
 window.addEventListener("load", () => {
-    loadMusic(musicIndex);
+    loadMusic(musicIndex);       //음악 재생   
+    playListMusic();            //리스트 초기화
 })
